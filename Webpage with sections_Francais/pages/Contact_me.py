f510d8688dd37af082f14cdeb3ec0fb22a98cd93
Kaviyarasu135 <kaviyarasu.fr@gmail.com>
2025-08-31 17:39:04 +0200

    Update Contact_me.py

--- Webpage with sections_Francais/pages/Contact_me.py
+++ Webpage with sections_Francais/pages/Contact_me.py
@@ -10,15 +10,17 @@ local_css("style/Style.css")
 # Original text translated into formal French
 st.header("Prenez contact avec moi", anchor=False)
 
-    contact_form = """
-    <form action="https://formsubmit.co/kaviyarasu.fr@gmail.com" method="POST">
-        <input type="hidden" name="_captcha" value="false">
-        <input type="text" name="name" placeholder="Votre nom" required>
-        <input type="email" name="email" placeholder="Votre adresse e-mail" required>
-        <textarea name="message" placeholder="Votre message ici" required></textarea>
-        <button type="submit">Envoyer</button>
-    </form>
-    """
+contact_form = """
+<form action="https://formsubmit.co/kaviyarasu.fr@gmail.com" method="POST">
+    <input type="hidden" name="_captcha" value="false">
+    <input type="text" name="name" placeholder="Votre nom" required>
+    <input type="email" name="email" placeholder="Votre adresse e-mail" required>
+    <textarea name="message" placeholder="Votre message ici" required></textarea>
+    <button type="submit">Envoyer</button>
+</form>
+"""
+
 st.write(contact_form, unsafe_allow_html=True)
+
 
 
