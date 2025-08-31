bcf9a8a6fd30ba90fb7c7c7069b908ee16441062
Kaviyarasu135 <kaviyarasu.fr@gmail.com>
2025-08-31 17:57:54 +0200

    Update Home.py

--- Webpage with sections_Francais/pages/Home.py
+++ Webpage with sections_Francais/pages/Home.py
@@ -22,13 +22,10 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
-# Path to the directory containing this script
-current_dir = Path(__file__).parent
-# Path to the project root (one level up from 'pages')
-project_root = current_dir.parent
+# Get the folder this script is in (pages/)
+BASE_DIR = Path(__file__).parent
 
-# Path to the image
-image_path = project_root / "Assets" / "RCL93391.jpg"
+profile_pic = BASE_DIR / "Assets" / "RCL93391.JPG"
 
 st.title("Kaviyarasu", anchor=False)
 st.write("Ingénieur industriel")
@@ -46,6 +43,7 @@ st.write(
         )
 st.subheader("Un ingénieur industriel de France", anchor=False)
 st.write("J’aide les entreprises à optimiser leurs processus de production en utilisant des modèles et diagrammes d’entreprise, tout en privilégiant la durabilité. En combinant des analyses basées sur les données avec l’automatisation, je rends les flux de travail plus efficaces tout en garantissant leur durabilité à long terme. J’utilise également Python pour développer des solutions qui simplifient les tâches et favorisent une prise de décision éclairée.", anchor=False)
+
 
 
 
