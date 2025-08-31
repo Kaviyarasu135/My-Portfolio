3f6bfcaf46d67f5772db69db8b6f17df9aecc793
Kaviyarasu135 <kaviyarasu.fr@gmail.com>
2025-08-31 17:48:04 +0200

    Update Contact_me.py

--- Webpage with sections_Francais/pages/Contact_me.py
+++ Webpage with sections_Francais/pages/Contact_me.py
@@ -14,7 +14,7 @@ local_css("Style/Style.css")
 st.header("Prenez contact avec moi", anchor=False)
 
 contact_form = """
-<form action="https://formsubmit.co/kaviyarasu.fr@gmail.com" method="POST">
+<form action="https://formsubmit.co/bdd6b1c863e3ab54935da1039d5473e7" method="POST">
     <input type="hidden" name="_captcha" value="false">
     <input type="text" name="name" placeholder="Votre nom" required>
     <input type="email" name="email" placeholder="Votre adresse e-mail" required>
@@ -24,6 +24,7 @@ contact_form = """
 """
 
 st.write(contact_form, unsafe_allow_html=True)
+
 
 
 
