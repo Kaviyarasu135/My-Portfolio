7e256e390a82178c263a535b9e4c8be254fc73b0
Kaviyarasu135 <kaviyarasu.fr@gmail.com>
2025-09-01 15:58:13 +0200

    Update Home.py

--- Webpage with sections_Francais/pages/Home.py
+++ Webpage with sections_Francais/pages/Home.py
@@ -40,9 +40,21 @@ with open(resume_file_francais, "rb") as pdf_file_francais:
 profile_pic = Image.open(profile_pic)
 
 col1, col2 = st.columns(2, gap="small")
+with col1:
+    
 with col2:
-    st.image(profile_pic, width=215)
-    if st.button(" Prenez contact avec moi"):#📧
+    
+    if st.button(" 📧Prenez contact avec moi"):#
+            st.switch_page("pages/Contact_me.py")
+
+with st.container():
+    left_column, right_column = st.columns(2)
+    with left_column:
+        st.image(profile_pic, width=215)
+    with right_column:
+        st.title("Kaviyarasu", anchor=False)
+        st.write("Ingénieur industriel")
+        if st.button("📧 Prenez contact avec moi"):
             st.switch_page("pages/Contact_me.py")
 
 st.title("Kaviyarasu", anchor=False)
@@ -61,6 +73,7 @@ st.write(
         )
 st.subheader("Un ingénieur industriel de France", anchor=False)
 st.write("J’aide les entreprises à optimiser leurs processus de production en utilisant des modèles et diagrammes d’entreprise, tout en privilégiant la durabilité. En combinant des analyses basées sur les données avec l’automatisation, je rends les flux de travail plus efficaces tout en garantissant leur durabilité à long terme. J’utilise également Python pour développer des solutions qui simplifient les tâches et favorisent une prise de décision éclairée.", anchor=False)
+
 
 
 
