d52822f1f36534d5ad951c899e9e629c9085e61d
Kaviyarasu135 <kaviyarasu.fr@gmail.com>
2025-09-02 21:22:04 +0200

    Update Core_Competencies.py

--- Webpage with sections_Francais/pages/Core_Competencies.py
+++ Webpage with sections_Francais/pages/Core_Competencies.py
@@ -20,29 +20,29 @@ st.title("Valeurs fondamentales", anchor=False)
 st.write("**1) Honnêteté :**")
 st.write(
     """
-    - Valorise la vérité avant le confort, recherchant des retours authentiques.
-    - Privilégie des relations honnêtes, même lorsque cela est difficile à entendre.
+    - Valoriser la vérité avant le confort, rechercher des retours authentiques.
+    - Privilégier des relations honnêtes, même lorsque cela est difficile à entendre.
     """
 )
 st.write("**2) Engagement :**")
 st.write(
     """
-    - S’investit pleinement dans ses objectifs, surmontant les défis avec détermination.
-    - Reste concentré et déterminé dans la poursuite de ses ambitions.
+    - S’investir pleinement dans ses objectifs, surmonter les défis avec détermination.
+    - Rester concentré et déterminé dans la poursuite de ses ambitions.
     """
 )
 st.write("**3) Discipline :**")
 st.write(
     """
-    - Croit en un effort constant et en la constance pour atteindre le succès.
-    - Adhère à des routines, faisant confiance au processus même dans les moments difficiles.
+    - Croire en un effort constant et en la constance pour atteindre le succès.
+    - Adhérer à des routines, faire confiance au processus même dans les moments difficiles.
     """
 )
 st.write("**4) Constance :**")
 st.write(
     """
-    - Se focalise sur le progrès à long terme plutôt que sur des résultats rapides.
-    - Atteint ses objectifs grâce à des améliorations quotidiennes constantes.
+    - Se focaliser sur le progrès à long terme plutôt que sur des résultats rapides.
+    - Atteindre ses objectifs grâce à des améliorations quotidiennes constantes.
     """
 )
 
@@ -80,6 +80,7 @@ df = pd.DataFrame({
 
 # Display the bar chart using Streamlit's built-in function
 st.bar_chart(df.set_index('Activités'))
+
 
 
 
