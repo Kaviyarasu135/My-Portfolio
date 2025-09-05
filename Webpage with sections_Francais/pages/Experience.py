2e434ebc148deab37478b8b7639fbd6f647031f7
Kaviyarasu135 <kaviyarasu.fr@gmail.com>
2025-09-05 11:15:07 +0200

    Update Experience.py

--- Webpage with sections_Francais/pages/Experience.py
+++ Webpage with sections_Francais/pages/Experience.py
@@ -26,42 +26,42 @@ st.write("**(Juillet 2023 - Mai 2024)**")
 st.write("**1) Programmation PLC et automatisation :**")
 st.write(
     """
-    - Développé et optimisé des programmes PLC à l’aide d’automates Beckhoff, configurant des modules d’entrée et de sortie pour améliorer les performances du système.
-    - Effectué le dépannage et le débogage des systèmes d’automatisation pour identifier et résoudre les problèmes matériels et logiciels.
-    - Contribué à l’intégration de systèmes de contrôle en temps réel, garantissant une communication fluide entre différents composants industriels.
+    - Développement et optimisation de programmes PLC à l’aide d’automates Beckhoff, Configuration de modules d’entrée et de sortie pour améliorer les performances du système.
+    - Réalisation du dépannage et le débogage des systèmes d’automatisation pour identifier et résoudre les problèmes matériels et logiciels.
+    - Contribution à l’intégration de systèmes de contrôle en temps réel, Garantie une communication fluide entre différents composants industriels.
     """
 )
 st.write("**2) Vérification et validation des composants du système :**")
 st.write(
     """
-    - Réalisé des tests et une validation des composants du système pour garantir leur conformité aux normes industrielles et aux exigences de sécurité opérationnelle.
-    - Contribué à effectuer des tests fonctionnels, des analyses d’erreurs et du débogage pour améliorer la fiabilité du système.
-    - Collaboré avec des équipes pluridisciplinaires pour affiner les spécifications de conception et vérifier le respect des critères de qualité et de performance.
+    - Réalisation des tests et une validation des composants du système pour garantir leur conformité aux normes industrielles et aux exigences de sécurité opérationnelle.
+    - Contribution à effectuer des tests fonctionnels, des analyses d’erreurs et du débogage pour améliorer la fiabilité du système.
+    - Collaboration avec des équipes pluridisciplinaires pour l’affinement des spécifications de conception et vérifier le respect des critères de qualité et de performance.
     """
 )
 st.write("**3) Durabilité et efficacité énergétique :**")
 st.write(
     """
-    - Contribué à des pratiques industrielles durables en analysant les processus à travers le prisme du triple bilan : impact économique, environnemental et social.
-    - Assisté dans l’identification de solutions d’automatisation rentables qui amélioraient l’efficacité opérationnelle tout en minimisant le gaspillage de ressources.
-    - Soutenu la recherche sur des matériaux écologiques et des stratégies de réduction des déchets pour aligner la production sur les objectifs de durabilité environnementale.
-    - Participé à des discussions sur la sécurité des travailleurs, l’ergonomie et l’automatisation éthique pour garantir que la technologie améliore les conditions de travail.
+    - Contribution à des pratiques industrielles durables Par l’analyse des processus à travers le prisme du triple bilan : impact économique, environnemental et social.
+    - Assistance à l’identification de solutions d’automatisation rentables Visant à améliorer l’efficacité opérationnelle tout en minimisant le gaspillage de ressources.
+    - Soutien à la recherche sur des matériaux écologiques et des stratégies de réduction des déchets pour aligner la production sur les objectifs de durabilité environnementale.
+    - Participation à des discussions sur la sécurité des travailleurs, l’ergonomie et l’automatisation éthique pour garantir que la technologie améliore les conditions de travail.
     """
 )
 st.write("**4) Gestion de la production et optimisation des flux de travail :**")
 st.write(
     """
-    - Acquis une expérience pratique en gestion de la production, se concentrant sur la planification des ressources, l’ordonnancement et l’optimisation des processus.
-    - Contribué à l’analyse des goulots d’étranglement dans les flux de production et proposé des améliorations pour accroître l’efficacité.
-    - Collaboré avec des équipes pluridisciplinaires pour garantir une intégration fluide des solutions d’automatisation dans les lignes de production existantes.
+    - Acquisition d'une expérience pratique en gestion de la production, se concentrant sur la planification des ressources, l’ordonnancement et l’optimisation des processus.
+    - Contribution à l’analyse des goulots d’étranglement dans les flux de production et Proposition d'améliorations pour accroître l’efficacité.
+    - Collaboration avec des équipes pluridisciplinaires pour assurer une intégration fluide des solutions d’automatisation dans les lignes de production existantes.
     """
 )
 st.write("**5) Diagrammes industriels et conception de systèmes :**")
 st.write(
     """
-    - Créé et interprété des diagrammes industriels, y compris des schémas électriques, des organigrammes et des plans de systèmes de contrôle.
-    - Assisté dans la conception et la documentation des systèmes d’automatisation, garantissant une représentation précise des composants électriques et mécaniques.
-    - Collaboré étroitement avec des ingénieurs et des concepteurs pour analyser et affiner les diagrammes industriels, améliorant la clarté et l’efficacité des systèmes.
+    - Création et interprétation des diagrammes industriels, y compris des schémas électriques, des organigrammes et des plans de systèmes de contrôle.
+    - Assistance à la conception et la documentation des systèmes d’automatisation, Garantie d'une représentation précise des composants électriques et mécaniques.
+    - Collaboration étroite avec des ingénieurs et des concepteurs Pour l’analyse et l’affinement des diagrammes industriels, améliorant la clarté et l’efficacité des systèmes.
     """
 )
 st.markdown("<h3><u>Sarmangal (Stagiaire en analyse) :</u></h3>", unsafe_allow_html=True)
@@ -115,5 +115,6 @@ st.write(
     - Participé à des discussions sur l’automatisation, la durabilité et la gestion de la production, contribuant à une approche holistique de l’efficacité industrielle.
     """
 )
+
 
 
