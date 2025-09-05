1ccb2eb817416ef0a42457d4f3ea2e855b693eae
Kaviyarasu135 <kaviyarasu.fr@gmail.com>
2025-09-05 10:55:15 +0200

    Update Skills.py

--- Webpage with sections_Francais/pages/Skills.py
+++ Webpage with sections_Francais/pages/Skills.py
@@ -23,50 +23,50 @@ st.markdown("<h3><u>Compétences personnelles :</u></h3>", unsafe_allow_html=Tru
 st.write("**1) Discipliné et engagé :**")
 st.write(
             """
-            - Maintient une approche cohérente et structurée pour atteindre ses objectifs, garantissant un progrès constant même dans des situations difficiles.
-            - Se concentre sur les solutions plutôt que sur les obstacles, restant dédié à l’amélioration continue.
-            - Privilégie la qualité et la précision, valorisant une exécution rigoureuse plutôt que des raccourcis.
+            - Maintenir une approche cohérente et structurée pour atteindre ses objectifs, garantir un progrès constant même dans des situations difficiles.
+            - Se concentrer sur les solutions plutôt que sur les obstacles, rester dédié à l’amélioration continue.
+            - Privilégier la qualité et la précision, valoriser une exécution rigoureuse plutôt que des raccourcis.
             """
         )
 st.write("**2) Orienté vers les objectifs :**")
 st.write(
             """
-            - Définit des objectifs clairs et suit un plan structuré pour les accomplir efficacement.
-            - Décompose les défis complexes en étapes gérables, garantissant un progrès constant.
-            - Aligne les objectifs sur un impact à long terme, équilibrant ambition et pragmatisme.
+            - Définir des objectifs clairs et suivre un plan structuré pour les accomplir efficacement.
+            - Décomposer les défis complexes en étapes gérables, garantir un progrès constant.
+            - Aligner les objectifs sur un impact à long terme, équilibrer ambition et pragmatisme.
             """
         )
 st.write("**3) Apprenant adaptable :**")
 st.write(
             """
-            - Assimile rapidement de nouveaux concepts et technologies, appliquant ses connaissances efficacement dans différents domaines.
-            - Considère les défis comme des opportunités de croissance, faisant preuve de flexibilité dans l’apprentissage.
-            - Relie les connaissances de divers domaines, améliorant la résolution de problèmes et la pensée stratégique.
+            - Assimiler rapidement de nouveaux concepts et technologies, appliquer ses connaissances efficacement dans différents domaines.
+            - Considérer les défis comme des opportunités de croissance, faire preuve de flexibilité dans l’apprentissage.
+            - Relier les connaissances de divers domaines, améliorer la résolution de problèmes et la pensée stratégique.
             """
         )
 st.write("**4) Honnête et transparent :**")
 st.write(
             """
-            - Valorise une communication claire et directe, garantissant confiance et responsabilité dans les interactions professionnelles.
-            - Reconnaît ses forces et ses axes d’amélioration, favorisant un développement continu.
-            - Encourage les retours honnêtes et une prise de décision pragmatique pour améliorer l’efficacité.
+            - Valoriser une communication claire et directe, garantir confiance et responsabilité dans les interactions professionnelles.
+            - Reconnaître ses forces et ses axes d’amélioration, favoriser un développement continu.
+            - Encourager les retours honnêtes et une prise de décision pragmatique pour améliorer l’efficacité.
             """
         )
 
 st.write("**5) Conscient de soi et stratégique :**")
 st.write(
             """
-            - Évalue les décisions en se concentrant sur les résultats à long terme et l’impact durable.
-            - Équilibre ambition et définition réaliste d’objectifs, garantissant une gestion efficace des ressources.
-            - Aborde les défis avec un esprit stratégique, s’adaptant aux situations évolutives tout en maintenant une clarté d’objectif.
+            - Évaluer les décisions en se concentrant sur les résultats à long terme et l’impact durable.
+            - Équilibrer ambition et définition réaliste d’objectifs, garantir une gestion efficace des ressources.
+            - Aborder les défis avec un esprit stratégique, s’adapter aux situations évolutives tout en maintenant une clarté d’objectif.
             """
         )
 st.write("**6) Résilient et persévérant :**")
 st.write(
             """
-            - Reste concentré sur les objectifs à long terme, persévérant face aux revers ou aux défis.
-            - Considère les obstacles comme des opportunités d’apprentissage et d’amélioration plutôt que des raisons d’abandon.
-            - Maintient une approche disciplinée, adaptant les stratégies si nécessaire sans perdre de vue l’objectif final.
+            - Rester concentré sur les objectifs à long terme, persévérer face aux revers ou aux défis.
+            - Considérer les obstacles comme des opportunités d’apprentissage et d’amélioration plutôt que des raisons d’abandon.
+            - Maintenir une approche disciplinée, adapter les stratégies si nécessaire sans perdre de vue l’objectif final.
             """
         )
 
@@ -74,49 +74,50 @@ st.markdown("<h3><u>Compétences techniques :</u></h3>", unsafe_allow_html=True)
 st.write("**1) Programmation PLC :**")
 st.write(
             """
-            - A acquis une expérience pratique avec les automates Beckhoff, configurant les systèmes d’entrée et de sortie pour optimiser l’automatisation industrielle.
-            - Apprécie le dépannage et le débogage des programmes PLC, garantissant un fonctionnement fluide et efficace des systèmes.
-            - Pense de manière systématique lors de la programmation, garantissant une logique claire, évolutive et alignée sur les exigences de production.
+            - Acquérir une expérience pratique avec les automates Beckhoff, configurer les systèmes d’entrée et de sortie pour optimiser l’automatisation industrielle.
+            - Apprécier le dépannage et le débogage des programmes PLC, garantir un fonctionnement fluide et efficace des systèmes.
+            - Penser de manière systématique lors de la programmation, garantir une logique claire, évolutive et alignée sur les exigences de production.
             """
         )
 st.write("**2) Systèmes industriels :**")
 st.write(
             """
-            - Comprend la nature interconnectée de l’automatisation, de la production et de l’optimisation des systèmes, garantissant des opérations fluides.
-            - Pense en termes de systèmes et de processus, appliquant des compétences analytiques pour améliorer l’efficacité et la fiabilité.
-            - Familier avec les diagrammes industriels, la validation des systèmes et l’optimisation des flux de travail, garantissant une documentation et une mise en œuvre claires.
+            - Comprendre la nature interconnectée de l’automatisation, de la production et de l’optimisation des systèmes, garantir des opérations fluides.
+            - Penser en termes de systèmes et de processus, appliquer des compétences analytiques pour améliorer l’efficacité et la fiabilité.
+            - Être familier avec les diagrammes industriels, la validation des systèmes et l’optimisation des flux de travail, garantir une documentation et une mise en œuvre claires.
             """
         )
 st.write("**3) Python :**")
 st.write(
             """
-            - Considère Python comme un outil puissant pour l’automatisation, l’analyse de données et l’optimisation des systèmes, l’appliquant à des tâches liées à l’industrie et à la durabilité.
-            - S’adapte rapidement aux nouveaux défis de programmation, tirant parti de la flexibilité de Python pour résoudre des problèmes dans l’automatisation industrielle et les flux de production.
-            - Utilise une approche structurée et analytique pour coder, garantissant des solutions efficaces, évolutives et bien documentées.
+            - Considérer Python comme un outil puissant pour l’automatisation, l’analyse de données et l’optimisation des systèmes, l’appliquer à des tâches liées à l’industrie et à la durabilité.
+            - S’adapter rapidement aux nouveaux défis de programmation, tirer parti de la flexibilité de Python pour résoudre des problèmes dans l’automatisation industrielle et les flux de production.
+            - Utiliser une approche structurée et analytique pour coder, garantir des solutions efficaces, évolutives et bien documentées.
             """
         )
 st.write("**4) Durabilité :**")
 st.write(
             """
-            - Aborde la durabilité avec un esprit pratique, se concentrant sur les aspects économiques, environnementaux et sociaux des systèmes industriels.
-            - Intéressé par la réduction des déchets, l’optimisation de l’utilisation des ressources et l’intégration de pratiques durables dans la production et l’automatisation.
-            - Considère la durabilité comme une stratégie à long terme, garantissant l’efficacité tout en tenant compte de l’impact global des processus industriels.
+            - Aborder la durabilité avec un esprit pratique, se concentrer sur les aspects économiques, environnementaux et sociaux des systèmes industriels.
+            - Être intéressé par la réduction des déchets, l’optimisation de l’utilisation des ressources et l’intégration de pratiques durables dans la production et l’automatisation.
+            - Considérer la durabilité comme une stratégie à long terme, garantir l’efficacité tout en tenant compte de l’impact global des processus industriels.
             """
         )
 st.write("**5) Gestion de la production :**")
 st.write(
             """
-            - Se concentre sur l’efficacité, l’organisation et l’amélioration continue, garantissant des flux de travail fluides et optimisés.
-            - Analyse les goulots d’étranglement dans la production et propose des améliorations stratégiques pour accroître la productivité.
-            - Comprend l’équilibre entre l’automatisation, la gestion des ressources et les facteurs humains dans les environnements de production.
+            - Se concentrer sur l’efficacité, l’organisation et l’amélioration continue, garantir des flux de travail fluides et optimisés.
+            - Analyser les goulots d’étranglement dans la production et proposer des améliorations stratégiques pour accroître la productivité.
+            - Comprendre l’équilibre entre l’automatisation, la gestion des ressources et les facteurs humains dans les environnements de production.
             """
         )
 st.write("**6) Gestion de projets :**")
 st.write(
             """
-            - Applique naturellement une pensée orientée vers les objectifs et structurée aux projets, garantissant clarté et concentration du début à la fin.
-            - Planifie et exécute les tâches de manière méthodique, équilibrant efficacité et adaptabilité dans la résolution de problèmes.
-            - Valorise une communication claire, des délais réalistes et une collaboration efficace, garantissant que les projets se déroulent sans heurts et atteignent leurs objectifs.
+            - Appliquer naturellement une pensée orientée vers les objectifs et structurée aux projets, garantir clarté et concentration du début à la fin.
+            - Planifier et exécuter les tâches de manière méthodique, équilibrer efficacité et adaptabilité dans la résolution de problèmes.
+            - Valoriser une communication claire, des délais réalistes et une collaboration efficace, garantir que les projets se déroulent sans heurts et atteignent leurs objectifs.
             """
 
         )
+
