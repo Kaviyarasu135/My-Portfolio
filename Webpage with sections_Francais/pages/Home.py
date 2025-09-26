54a45fccb40257e7c68b1e722df66c84f9d1bbd6
Kaviyarasu135 <kaviyarasu.fr@gmail.com>
2025-09-26 10:21:22 +0200

    Update Home.py

--- Webpage with sections_Francais/pages/Home.py
+++ Webpage with sections_Francais/pages/Home.py
@@ -50,7 +50,9 @@ st.write(
             """
         )
 st.subheader("Un ingénieur industriel en France", anchor=False)
-st.write("J’aide les entreprises à optimiser leurs processus de production en utilisant des modèles et diagrammes d’entreprise, tout en privilégiant la durabilité. En combinant des analyses basées sur les données avec l’automatisation, je rends les flux de travail plus efficaces tout en garantissant leur durabilité à long terme. J’utilise également Python pour développer des solutions qui simplifient les tâches et favorisent une prise de décision éclairée.", anchor=False)
+st.write(
+    "J’aide les entreprises à optimiser leurs processus de production en utilisant des modèles et diagrammes d’entreprise, tout en privilégiant la durabilité. En combinant des analyses basées sur les données avec l’automatisation, je rends les flux de travail plus efficaces tout en garantissant leur durabilité à long terme. J’utilise également Python pour développer des solutions qui simplifient les tâches et favorisent une prise de décision éclairée."
+)
 
 
 
